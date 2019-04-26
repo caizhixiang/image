431605466e0e06e999d5bde77f61240b9319741c
caizhixiang <caizhixiang1989@163.com>
2019-04-26 14:03:02 +0800

    更改配置适合springboot打war包部署到外面tomcat服务器上

--- src/main/java/com/caizhixiang/springboot/ImageApplication.java
+++ src/main/java/com/caizhixiang/springboot/ImageApplication.java
@@ -3,13 +3,19 @@ package com.caizhixiang.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan({"com.caizhixiang.springboot.mapper"})
-public class ImageApplication {
+public class ImageApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ImageApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ImageApplication.class);
     }
 
 }
