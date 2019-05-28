032d17c2b874d506a8fe270d16c2348ba306543f
caizhixiang <caizhixiang1989@163.com>
2019-05-28 18:49:13 +0800

    前台图片展示页面

--- src/main/java/com/caizhixiang/springboot/web/ApiController.java
+++ src/main/java/com/caizhixiang/springboot/web/ApiController.java
@@ -1,9 +1,14 @@
 package com.caizhixiang.springboot.web;
 
+import com.caizhixiang.springboot.mapper.entity.Image;
+import com.caizhixiang.springboot.service.ImageService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * @author caizhixiang
@@ -15,9 +20,15 @@ import org.springframework.web.servlet.ModelAndView;
 @Slf4j
 @RequestMapping("/api")
 public class ApiController {
+    @Autowired
+    private ImageService imageService;
+    @RequestMapping("/index")
     public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("index");
+        List<Image> images = imageService.findAll();
+
+        modelAndView.addObject("images", images);
+        modelAndView.setViewName("api/index");
         return modelAndView;
     }
 }
